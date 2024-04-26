cdb8ac46ae6b6e1c59a55c5124d52565ddd0259d
m3lon4rt <m3lon0337@gmail.com>
2024-04-26 19:32:51 +0800

    Setup DB connection

--- AI/server.py
+++ AI/server.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from src.main.companyranking.company_controller import company_bp
 from src.main.companysuggestion.companysuggestion_controller import companysuggestion_bp
+from util.database import get_db, close_db
 
 app = Flask(__name__)
+
+# Initialize database
+@app.before_first_request
+def before_first_request_func():
+    init_db()
+
+# Register teardown function to close database connection
+@app.teardown_appcontext
+def teardown_appcontext_func(exception=None):
+    close_db()
 
 # Register blueprints
 app.register_blueprint(company_bp, url_prefix='/api/')
